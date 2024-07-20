82589f0802b7e61d7aa897684f0d00fdf9c29461
Somaiah <somaiapalaparthi@gmail.com>
2024-07-20 12:17:33 +0530

    added files

---
+++ exportfile.mjs
@@ -0,0 +1,8 @@
+
+export let mul=(a,b)=>{
+    return a*b;
+}
+
+export let add=(a,b)=>{
+    return a+b;
+}
---
+++ importfile.mjs
@@ -0,0 +1,6 @@
+import {mul,add} from "./exportfile.mjs";
+
+console.log(mul(10,3));
+console.log(add(10,3));
+
+
